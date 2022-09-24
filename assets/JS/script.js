46a46b6562f84ab561eb4672e3c6aaabdbd1f319
root <sedrasabbagh33@gmail.com>
2022-09-24 10:58:02 +0300

    add special courses on excel tab

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,18 +1,32 @@
-let courses = document.querySelectorAll('.carousel-cards .carousel-item');
+// let tabs = document.querySelectorAll('button[data-tab]');
+let tabs = Array.from(document.querySelectorAll('button[data-tab]'));
+// console.log(tabs);
 
-courses.forEach((el) => {
-    const minPerSlide=4;
-    let next=el.nextElementSibling;
-    for (var i = 1; i <minPerSlide; i++) {
+tabs.map(tab=>   {  
+    // console.log(tab)
+    tabs[0].click();
+    tab.addEventListener('click', function(){
+        // console.log(this);
+        let selector =`.special-courses-${this.dataset.tab} .carousel-item`;
+        console.log(selector);
+        let courses = document.querySelectorAll(selector);
+
+        courses.forEach((el) => {
+        const minPerSlide=4;
+        let next=el.nextElementSibling;
+        for (var i = 1; i <minPerSlide; i++) {
         if(!next)
         {
-            next=courses[0];
+        next=courses[0];
         }
         
         let cloneChild =next.cloneNode(true);
         el.appendChild(cloneChild.children[0]);
         next=next.nextElementSibling;
-    }
+        }        
+        });
+    })
+    });
 
 
-});
+
